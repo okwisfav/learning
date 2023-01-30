ab77e0d5864c7b883872deaa11eba6ab96e3ad56
ofego favour <ofegofavour@gmail.com>
2023-01-30 13:17:05 +0100

    async

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Learning from './learning/Learning'
 import Setup from './tutorial/6-useReducer/setup/index'
+ 
 function App() {
   return (
     <div className='container'>
-
+        <Learning />
     </div>
   )
 }
